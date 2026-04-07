d34f55e947480490df39887be5d7d457471e22cb
DearingDev <jdearing10@gmail.com>
2026-04-07 09:12:30 -0500

    Updating pre-import script to properly load RTSession.

--- RTShell/internal/scripts/preimport.ps1
+++ RTShell/internal/scripts/preimport.ps1
@@ -1,6 +1,6 @@
 ﻿# Place all code that should be run before functions are imported here
 # Load RTSession configuration
-. "$PSScriptRoot\..\functions\RTSession.ps1"
+. "$script:ModuleRoot\internal\functions\RTSession.ps1"
 
 # Module-scope session state
 $Script:RTSession = [RTSession]::new()
